dc336ecc12a378b43fb27c0ec8fa9e65e0d01129
vibhorvimal12 <vibhorvimal69@gmail.com>
2024-06-13 12:24:25 +0000

    math.js

---
+++ variable.js/maths.js
@@ -0,0 +1,23 @@
+//console.log(Math);
+/*
+console.log(Math.abs(-8));
+console.log(Math.ceil(9,8));
+console.log(Math.round(12.6));
+console.log(Math.floor(3.6));
+console.log(Math.sqrt(6));
+console.log(Math.min(6,1, 2, 11));
+console.log(Math.max(6, 5,10));
+*/
+
+
+console.log(Math.random()) // it always  gives values b/w 0 to 1
+console.log(Math.random()*10);
+console.log(Math.random()*10 + 1);
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)));
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
